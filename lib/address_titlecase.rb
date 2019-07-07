069233e2e29c4839e542f92ebfe79f5f205a4dec
juliannaroen <jsroen@gmail.com>
2019-07-07 14:11:38 -0700

    Initialize gem

---
+++ lib/address_titlecase.rb
@@ -0,0 +1,6 @@
+require "address_titlecase/version"
+
+module AddressTitlecase
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/address_titlecase/version.rb
@@ -0,0 +1,3 @@
+module AddressTitlecase
+  VERSION = "0.1.0"
+end
